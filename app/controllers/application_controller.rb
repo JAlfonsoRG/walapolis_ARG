38cc1ad84c52efa7ff3568389247cb032edf202b
JAlfonsoRG <ramir.alfonsorg@gmail.com>
2018-05-01 20:50:19 -0500

    Registration new and Session new views has been edited on style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,15 @@ class ApplicationController < ActionController::Base
 
     #Entre las 5:00 AM a las 11:59 AM => "BUENOS DÍAS"
     if current_hour > 5 && current_hour < 12
-      @greeting = "Buenos Dias"
+      @greeting = "¡Buenos Dias!"
 
     #Entre las 12:00 PM a las 8:00 PM => "BUENAS TARDES"
     elsif current_hour > 12 && current_hour < 20
-      @greeting = "Buenas Tardes"
+      @greeting = "¡Buenas Tardes!"
 
     #Entre las 7:31 PM a las 4:59 PM => "BUENAS NOCHES"
     else
-      @greeting = "Buenas Noches"        
+      @greeting = "¡Buenas Noches!"        
     end
   end
 
