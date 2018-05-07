0ffaae9b38bacc1eaa63d9a9bb70ee9fe44cdad8
JAlfonsoRG <ramir.alfonsorg@gmail.com>
2018-05-07 13:09:06 -0500

    Active storage has been installed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
     current_hour = Time.zone.now.hour
 
     #Entre las 5:00 AM a las 11:59 AM => "BUENOS DÍAS"
-    if current_hour > 5 && current_hour < 12
+    if current_hour >= 5 && current_hour < 12
       @greeting = "¡Buenos Dias!"
 
     #Entre las 12:00 PM a las 8:00 PM => "BUENAS TARDES"
-    elsif current_hour > 12 && current_hour < 20
+    elsif current_hour >= 12 && current_hour < 20
       @greeting = "¡Buenas Tardes!"
 
     #Entre las 7:31 PM a las 4:59 PM => "BUENAS NOCHES"
